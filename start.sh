70e085642f303137c4d0750bca9963336594519a
kawamoto <bahihzss@gmail.com>
2021-12-18 04:01:54 +0900

    id_rsa をシークレットからマウントして使用するようの変更

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-echo "$DATABASE_SSH_PRIVATE_KEY" > id_rsa.pem
-ssh -N -L 3306:$DATABASE_HOST:3306 ec2-user@$DATABASE_SSH_HOST -i id_rsa.pem
+ssh -N -L 3306:$DATABASE_HOST:3306 ec2-user@$DATABASE_SSH_HOST -i /secret/id_rsa.pem
 
 yarn prisma:generate
 yarn prisma:migrate
