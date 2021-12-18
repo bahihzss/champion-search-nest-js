ac77469a98de254f60788492d6f338fcf69e0656
kawamoto <bahihzss@gmail.com>
2021-12-18 11:54:51 +0900

    秘密鍵の受け渡しをファイルマウントから環境変数に変更

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-ssh -o ExitOnForwardFailure=yes -f -N -L 3306:${DATABASE_HOST}:3306 ec2-user@${DATABASE_SSH_HOST} -i /secret/id_rsa.pem sleep 15
+echo "${DATABASE_SSH_KEY}" > ./id_rsa
+chmod 600 ./id_rsa
+ssh -o ExitOnForwardFailure=yes -f -N -L 3306:${DATABASE_HOST}:3306 ec2-user@${DATABASE_SSH_HOST} -i ./id_rsa.pem sleep 15
 
 yarn prisma:generate
 yarn prisma:migrate
