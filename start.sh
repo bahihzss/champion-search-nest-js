9a708b8a3d300afca368ba791a1f286bb7d414dc
kawamoto <bahihzss@gmail.com>
2021-12-18 11:42:28 +0900

    SSH トンネルをバックグラウンドで行うように変更

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-ssh -N -L 3306:$DATABASE_HOST:3306 ec2-user@$DATABASE_SSH_HOST -i /secret/id_rsa.pem
+ssh -o ExitOnForwardFailure=yes -f -N -L 3306:${DATABASE_HOST}:3306 ec2-user@${DATABASE_SSH_HOST} -i /secret/id_rsa.pem sleep 15
 
 yarn prisma:generate
 yarn prisma:migrate
